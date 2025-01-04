47ebeb91ead45289533c347c93525c19f1b29a17
gkarabelos <gkarabelos@outlook.com>
2025-01-04 18:39:16 +0200

    Implement the Validations

---
+++ Technico.API/Validations/Owner/CreateOwnerValidation.cs
@@ -0,0 +1,43 @@
+﻿using FluentValidation;
+
+namespace Technico.API.Validations.Owner
+{
+    public class CreateOwnerValidation : AbstractValidator<CreateOwnerDto>
+    {
+        public CreateOwnerValidation()
+        {
+            RuleFor(o => o.VatNumber)
+                .NotEmpty().WithMessage("VAT Number is required.")
+                .Matches("^[0-9]+$").WithMessage("VAT Number must contain only digits.")
+                .MaximumLength(15).WithMessage("VAT Number cannot exceed 15 digits.");
+
+            RuleFor(o => o.Name)
+                .NotEmpty().WithMessage("Name is required.")
+                .MaximumLength(50).WithMessage("Name cannot exceed 50 characters.");
+
+            RuleFor(o => o.Surname)
+                .NotEmpty().WithMessage("Surname is required.")
+                .MaximumLength(50).WithMessage("Surname cannot exceed 50 characters.");
+
+            RuleFor(o => o.Address)
+                .NotEmpty().WithMessage("Address is required.")
+                .MaximumLength(200).WithMessage("Address cannot exceed 200 characters.");
+
+            RuleFor(o => o.PhoneNumber)
+                .NotEmpty().WithMessage("Phone number is required.")
+                .Matches(@"^\+?\d+$").WithMessage("Phone number must contain only numbers and optional '+' prefix.")
+                .MaximumLength(20).WithMessage("Phone Number cannot exceed 20 characters.");
+
+            RuleFor(o => o.Email)
+                .NotEmpty().WithMessage("Email is required.")
+                .EmailAddress().WithMessage("Invalid email format.");
+
+            RuleFor(o => o.PasswordHash)
+                .NotEmpty().WithMessage("Password is required.")
+                .MinimumLength(8).WithMessage("Password must be at least 8 characters long.");
+
+            RuleFor(o => o.Type)
+                .IsInEnum().WithMessage("Type of user must be 0 (Admin) or 1 (Owner).");
+        }
+    }
+}
---
+++ Technico.API/Validations/Repair/CreateRepairValidation.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+
+namespace Technico.API.Validations.Repair
+{
+    public class CreateRepairValidation : AbstractValidator<CreateRepairDto>
+    {
+        public CreateRepairValidation()
+        {
+            RuleFor(r => r.Date)
+                .NotEmpty().WithMessage("Date is required.");
+
+            RuleFor(r => r.Type)
+                .NotEmpty().WithMessage("Repair type is required.")
+                .MaximumLength(50).WithMessage("Repair type cannot exceed 50 characters.");
+
+            RuleFor(r => r.Description)
+                .MaximumLength(500).WithMessage("Description cannot exceed 500 characters.");
+
+            RuleFor(r => r.Cost)
+                .GreaterThanOrEqualTo(0).WithMessage("Repair cost must be a positive value.")
+                .PrecisionScale(10, 2, true).WithMessage("Repair cost must have up to 10 digits with 2 decimal places.");
+
+            RuleFor(r => r.PropertyId)
+                .NotEmpty().WithMessage("Property ID is required.");
+        }
+    }
+}
