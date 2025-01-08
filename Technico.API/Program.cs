40b050b1f25482939a9fcc41658472ca2f92d591
gkarabelos <gkarabelos@outlook.com>
2025-01-08 11:13:55 +0200

    Update the Program.cs

--- Technico.API/Program.cs
+++ Technico.API/Program.cs
@@ -1,4 +1,9 @@
 
+using Microsoft.EntityFrameworkCore;
+using Technico.API.Validations.Owner;
+using Technico.API.Validations.Property;
+using Technico.API.Validations.Repair;
+
 namespace Technico.API
 {
     public class Program
@@ -9,10 +14,48 @@ namespace Technico.API
 
             // Add services to the container.
 
+            var configuration = builder.Configuration;
+
+            builder.Services.AddDbContextPool<TechnicoDbContext>(options =>
+            {
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DbContext"),
+                provideroptions => provideroptions.EnableRetryOnFailure());
+            });
+
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            builder.Services.AddFluentValidationAutoValidation();
+            builder.Services.AddFluentValidationClientsideAdapters();
+
+            builder.Services.AddValidatorsFromAssemblyContaining<CreateOwnerValidation>();
+            builder.Services.AddValidatorsFromAssemblyContaining<CreatePropertyValidation>();
+            builder.Services.AddValidatorsFromAssemblyContaining<CreateRepairValidation>();
+            builder.Services.AddValidatorsFromAssemblyContaining<UpdateOwnerValidation>();
+            builder.Services.AddValidatorsFromAssemblyContaining<UpdatePropertyValidation>();
+            builder.Services.AddValidatorsFromAssemblyContaining<UpdateRepairValidation>();
+
+            builder.Services.AddScoped<IOwnerRepository, OwnerRepository>();
+            builder.Services.AddScoped<IOwnerService, OwnerService>();
+            builder.Services.AddScoped<IPropertyRepository, PropertyRepository>();
+            builder.Services.AddScoped<IPropertyService, PropertyService>();
+            builder.Services.AddScoped<IRepairRepository, RepairRepository>();
+            builder.Services.AddScoped<IRepairService, RepairService>();
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAngularApp", policy =>
+                {
+                    policy.WithOrigins("http://localhost:4200")
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+                });
+            });
 
             var app = builder.Build();
 
@@ -25,8 +68,9 @@ namespace Technico.API
 
             app.UseHttpsRedirection();
 
-            app.UseAuthorization();
+            app.UseCors("AllowAngularApp");
 
+            app.UseAuthorization();
 
             app.MapControllers();
 
